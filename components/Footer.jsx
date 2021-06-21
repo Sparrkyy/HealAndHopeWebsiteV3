c8445243bf60270468e2a07c64ffbe2589d99508
useStateEthan <ethancampbellhodges@gmail.com>
2021-06-21 10:50:49 -0400

    added footer compoenent

---
+++ components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer>
+			<a className='footer-trademark' href='#'>
+				Copyright © 2021 heal and hope | www.healandhope.net | All Rights
+				Reserved{' '}
+			</a>
+			<p className='footer-email'>healandhopewebmaster@gmail.com</p>
+		</footer>
+	);
+};
+
+export default Footer;
